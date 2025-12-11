6f84ebb0857ad58b8652a99ead6fba41261dc7dd
VoidEngineCC <anexious666@gmail.com>
2025-12-11 11:57:31 +0300

    Replace VDCore with QBCore for player handling

--- server.lua
+++ server.lua
@@ -1,19 +1,16 @@
-local VDCore = exports['vd-core']:GetCoreObject()
+local QBCore = exports['qb-core']:GetCoreObject()
 
--- Your whitelisted license(s) - MUST MATCH CLIENT
 local whitelistedLicenses = {
-    "license:b1ecf5348bd4d5b3abd09e0272a97059d93e9c8d", -- REPLACE THIS WITH YOUR LICENSE
+    "license:xxxx", -- license
 }
 
--- Log executions
 RegisterServerEvent('eventtester:logExecution')
 AddEventHandler('eventtester:logExecution', function(data)
     local src = source
-    local player = VDCore.Functions.GetPlayer(src)
+    local player = QBCore.Functions.GetPlayer(src)
     
     if not player then return end
     
-    -- Check license
     local isAllowed = false
     for _, allowedLicense in ipairs(whitelistedLicenses) do
         if player.PlayerData.license == allowedLicense then
@@ -31,7 +28,6 @@ AddEventHandler('eventtester:logExecution', function(data)
         return
     end
     
-    -- Log the execution
     print(string.format('^3[Event Tester] %s (ID: %s) executed:^0', 
         player.PlayerData.name, 
         src
@@ -43,24 +39,3 @@ AddEventHandler('eventtester:logExecution', function(data)
     end
     print('^3----------------------------------^0')
 end)
-
--- Command to get license
-VDCore.Commands.Add('getlicense', 'Get your license ID', {}, false, function(source)
-    local src = source
-    local player = VDCore.Functions.GetPlayer(src)
-    if player then
-        TriggerClientEvent('VDCore:Notify', src, 'Your license: ' .. player.PlayerData.license, 'success')
-        print('^3[License] ' .. player.PlayerData.name .. ': ' .. player.PlayerData.license .. '^0')
-    end
-end, 'admin')
-
--- Debug command to test server side
-VDCore.Commands.Add('testevent', 'Test event from server', {}, false, function(source)
-    local src = source
-    local player = VDCore.Functions.GetPlayer(src)
-    
-    if player then
-        print('^2[Test] Player ' .. player.PlayerData.name .. ' executed test event^0')
-        TriggerClientEvent('VDCore:Notify', src, 'Test event executed from server', 'success')
-    end
-end, 'admin')
